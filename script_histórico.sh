217e17894806218769e6ebb51422be8c854a5f9e
renatata_a_rata <129771759+Renatata-H@users.noreply.github.com>
2023-10-20 21:26:17 -0300

    Update script_histórico.sh

--- script_histórico.sh
+++ script_histórico.sh
@@ -20,12 +20,12 @@ eh_status () {
 		frase="$frase "
 	fi
 	
-	if [ "${frase:0:2}" = "MA" ] || [ "${frase:0:2}" = " A" ] || [ "${frase:0:2}" = "P " ] || [ "${frase:0:2}" = "T " ] || [ "${frase:0:2}" = "I " ] || [ "${frase:0:2}" = "IP" ] || [ "${frase:0:2}" = "IR" ] || [ "${frase:0:2}" = "IT" ] || [ "${frase:0:2}" = "IL" ] || [ "${frase:0:2}" = "RA" ] || [ "${frase:0:2}" = "RN" ] || [ "${frase:0:2}" = "RF" ] || [ "${frase:0:2}" = "AE" ]; then
+	if [ "${frase:0:2}" = "MA" ] || [ "${frase:0:2}" = " A" ] || [ "${frase:0:2}" = "P " ] || [ "${frase:0:2}" = "T " ] || [ "${frase:0:2}" = "I " ] || [ "${frase:0:2}" = "IP" ] || [ "${frase:0:2}" = "IR" ] || [ "${frase:0:2}" = "IT" ] || [ "${frase:0:2}" = "IL" ] || [ "${frase:0:2}" = "RA" ] || [ "${frase:0:2}" = "RN" ] || [ "${frase:0:2}" = "RF" ] || [ "${frase:0:2}" = "AE" ] || [ "${frase:0:2}" = "DI" ]; then
 	 	status=0
 	fi
 	
 	echo $status
-}
+	}
 
 declare -a lista_disciplinas
 declare -a lista_status
@@ -34,8 +34,8 @@ printf "Digite o nome do arquivo de histórico escolar (sem .pdf): "
 read nome_do_arquivo
 nome_do_arquivo="./"$nome_do_arquivo
 
-pdftotext -f 2 $nome_do_arquivo".pdf"		# Cria-se o arquivo em .txt, a partir da segunda página.
-#printf "\nFIM" >> $nome_do_arquivo".txt"	# Coloca-se a flag "FIM" ao fim. Necessário?
+pdftotext -f 2 $nome_do_arquivo".pdf"					# Cria-se o arquivo em .txt, a partir da segunda página.
+#printf "\nFIM" >> $nome_do_arquivo".txt"				# Coloca-se a flag "FIM" ao fim. Necessário?
 
 # Índices para as listas "lista_disciplinas" e "lista_status"
 i=0
@@ -45,7 +45,7 @@ j=0
 while read -r linha; do
 
 	# Pega nomes das disciplinas
-	if [ ${#linha} -eq 7 ]; then 		# Nota: precisa conferir se as primeiras letras são siglas?
+	if [ ${#linha} -eq 7 ]; then 						# Nota: precisa conferir se as primeiras letras são siglas?
 		lista_disciplinas["${i}"]="$linha"
 		i=$i+1;
 	fi
@@ -67,16 +67,270 @@ while read -r linha; do
 		j=$j+1;
 	fi
 	
-done < $nome_do_arquivo".txt"
-# No final do laço, remove-se o .txt, pois não será mais necessário.
-rm -f $ $nome_do_arquivo".txt"		
+	done < $nome_do_arquivo".txt"
+rm -f $ $nome_do_arquivo".txt"						# No final do laço, remove-se o .txt, pois não será mais necessário.
 
-
-
-
-
-
+# Apenas para depuração
 echo "${lista_disciplinas[*]}"
 echo "${lista_status[*]}"
+
+quantidade_disciplinas="$((${#lista_disciplinas[@]}-1))"
+sistemas=("0" "0" "0")	# Desenvolvimento de software, sistemas paralelos e banco de dados --> (2, 2, 1) + 2 --> SOMA: 7
+ia=("0" "0" "0" "0") # IA, introdução à IA, sistemas, teoria --> (1, 2, 2, 1) --> SOMA: 6
+dados=("0" "0" "0" "0") # Nucleos 1, 2, 3, 4 --> (4, 1, 1, 1) --> SOMA: 7
+teoriaOBR=("0" "0" "0") # Algoritmos, otimização, matemática discreta --> (2, 2, 3) --> SOMA 7 ****** área esquisita
+teoriaOPT=("0" "0" "0") # Os "II".
+
+i=0
+while [ $i -le $quantidade_disciplinas ]; do
+	
+	nome_disciplina="${lista_disciplinas[${i}]}"
+	status_disciplina="${lista_status[${i}]}"
+
+	if [ "$status_disciplina" = "A" ] || [ "$status_disciplina" = "AE" ]; then	# Seria DI válido também?
+
+		#printf $nome_disciplina" "
+		#printf $status_disciplina"\n"
+		codigo_disciplina="${nome_disciplina:3:4}"
+		printf $codigo_disciplina
+
+		############ MAE
+		if [ "${nome_disciplina:0:3}" = "MAE" ]; then
+			
+			# MAE0221 - Probabilidade I
+			if [ "$codigo_disciplina" = "0221" ]; then
+				ia[3]="$((${ia[3]}+1))"			
+				dados[0]="$((${dados[0]}+1))"	
+				teoriaOPT[2]="$((${teoriaOPT[2]}+1))"	
+
+			# MAE0515 - Introdução à Teoria dos Jogos
+			elif [ "$codigo_disciplina" = "0515" ]; then
+				ia[3]="$((${ia[3]}+1))"	
+
+			# MAE0312 - Introdução aos Processos Estocásticos
+			elif [ "$codigo_disciplina" = "0312" ]; then
+				dados[1]="$((${dados[1]}+1))"	
+
+			# MAE0228 - Noções de Probabilidade e Processos Estocásticos
+			elif [ "$codigo_disciplina" = "0228" ]; then
+				dados[1]="$((${dados[1]}+1))"	
+				teoriaOPT[2]="$((${teoriaOPT[2]}+1))"	
+
+			# MAE0580 - Introdução à aprendizagem estatística
+			elif [ "$codigo_disciplina" = "0580" ]; then
+				dados[1]="$((${dados[1]}+1))"	
+
+			# MAE224 - Probabilidade II
+			elif [ "$codigo_disciplina" = "0224" ]; then
+				teoriaOPT[2]="$((${teoriaOPT[2]}+1))"	
+
+			# MAE0326 - Aplicações de Processos Estocásticos
+			elif [ "$codigo_disciplina" = "0326" ]; then
+				teoriaOPT[2]="$((${teoriaOPT[2]}+1))"
+			fi
+
+
+
+		############ MAT
+		elif [ "${nome_disciplina:0:3}" = "MAT" ]; then
+			
+			# MAT0349 - Introdução à Lógica
+			if [ "$codigo_disciplina" = "0349" ]; then
+				ia[3]="$((${ia[3]}+1))"	
+
+			# MAT0206 - Análise Real	
+			if [ "$codigo_disciplina" = "0206" ]; then
+				teoriaOBR[2]="$((${teoriaOBR[2]]}+1))"	
+
+			# MAT0264 - Anéis e Corpos
+			elif [ "$codigo_disciplina" = "0264" ]; then
+				teoriaOBR[2]="$((${teoriaOBR[2]]}+1))"	
+
+			# MAT0225 - Funções Analíticas
+			elif [ "$codigo_disciplina" = "0264" ]; then
+				teoriaOPT[2]="$((${teoriaOPT[2]]}+1))"	
+
+			# MAT0234 - Medida e Integração
+			elif [ "$codigo_disciplina" = "0234" ]; then
+				teoriaOPT[2]="$((${teoriaOPT[2]]}+1))"	
+
+			# MAT0265 - Grupos
+			elif [ "$codigo_disciplina" = "0234" ]; then
+				teoriaOPT[2]="$((${teoriaOPT[2]]}+1))"	
+
+			# MAT0311 - Cálculo Diferencial e Integral V
+			elif [ "$codigo_disciplina" = "0311" ]; then
+				teoriaOPT[2]="$((${teoriaOPT[2]]}+1))"	
+
+			fi
+
+
+		############ MAC
+		elif [ "${nome_disciplina:0:3}" = "MAC" ]; then
+			
+			# MAC0218 - Técnicas de Programação II
+			if [ "$codigo_disciplina" = "0349" ]; then
+				sistemas[0]="$((${sistemas[0]}+1))"
+				ia[2]="$((${ia[2]}+1))"
+			
+			# MAC0332 - Engenharia de Software
+			elif [ "$codigo_disciplina" = "0332" ]; then
+				sistemas[0]="$((${sistemas[0]}+1))"
+				ia[2]="$((${ia[2]}+1))"
+
+			# MAC0346 - Programação para Jogos Digitais
+			elif [ "$codigo_disciplina" = "0346" ]; then
+				sistemas[0]="$((${sistemas[0]}+1))"
+
+			# MAC0413 - Tópicos Avançados de Programação Orientada a Objetos
+			elif [ "$codigo_disciplina" = "0413" ]; then
+				sistemas[0]="$((${sistemas[0]}+1))"
+				ia[2]="$((${ia[2]}+1))"
+
+			# MAC0467 - Empreendedorismo Digital
+			elif [ "$codigo_disciplina" = "0467" ]; then
+				sistemas[0]="$((${sistemas[0]}+1))"
+
+			# MAC0470 - Desenvolvimento de Software Livre
+			elif [ "$codigo_disciplina" = "0470" ]; then
+				sistemas[0]="$((${sistemas[0]}+1))"
+
+			# MAC0472 - Laboratório de Métodos Ágeis
+			elif [ "$codigo_disciplina" = "0472" ]; then
+				sistemas[0]="$((${sistemas[0]}+1))"
+				ia[2]="$((${ia[2]}+1))"
+
+			# MAC0475 - Laboratório de Sistemas Computacionais Complexos
+			elif [ "$codigo_disciplina" = "0470" ]; then
+				sistemas[0]="$((${sistemas[0]}+1))"
+
+			# MAC0219 - Programação Concorrente e Paralela
+			elif [ "$codigo_disciplina" = "0219" ]; then
+				sistemas[1]="$((${sistemas[1]}+1))"
+				dados[3]="$((${dados[3]}+1))"
+
+			# MAC0344 - Arquitetura de Computadores
+			elif [ "$codigo_disciplina" = "0344" ]; then
+				sistemas[1]="$((${sistemas[1]}+1))"
+
+			# MAC0352 - Redes de Computadores e Sistemas Distribuídos
+			elif [ "$codigo_disciplina" = "0352" ]; then
+				sistemas[1]="$((${sistemas[1]}+1))"
+
+			# MAC0463 - Computação Móvel
+			elif [ "$codigo_disciplina" = "0463" ]; then
+				sistemas[1]="$((${sistemas[1]}+1))"
+
+			# MAC0426 - Sistemas de Bancos de Dados
+			elif [ "$codigo_disciplina" = "0426" ]; then
+				sistemas[2]="$((${sistemas[2]}+1))"
+				dados[0]="$((${dados[0]}+1))"
+
+			# MAC0439 - Laboratório de Bancos de Dados
+			elif [ "$codigo_disciplina" = "0439" ]; then
+				sistemas[2]="$((${sistemas[2]}+1))"
+
+			# MAC0459 - Ciência e Engenharia de Dados
+			elif [ "$codigo_disciplina" = "0459" ]; then
+				sistemas[2]="$((${sistemas[2]}+1))"
+				ia[1]="$((${ia[1]}+1))"
+
+			# MAC0425 - Inteligência Artificial
+			elif [ "$codigo_disciplina" = "0425" ]; then
+				ia[0]="$((${ia[0]}+1))"
+
+			# MAC0318 - Introdução à Programação de Robôs Móveis
+			elif [ "$codigo_disciplina" = "0318" ]; then
+				ia[1]="$((${ia[1]}+1))"
+
+			# MAC0444 - Sistemas Baseados em Conhecimento
+			elif [ "$codigo_disciplina" = "0444" ]; then
+				ia[1]="$((${ia[1]}+1))"
+
+			# MAC0460 - Introdução ao aprendizado de máquina
+			elif [ "$codigo_disciplina" = "0460" ]; then
+				ia[1]="$((${ia[1]}+1))"
+				dados[0]="$((${dados[0]}+1))"
+
+			# MAC0414 - Autômatos, Computabilidade e Complexidade
+			elif [ "$codigo_disciplina" = "0414" ]; then
+				ia[3]="$((${ia[3]}+1))"
+				teoriaOBR[0]="$((${teoriaOBR[0]}+1))"
+				teoriaOPT[2]="$((${teoriaOPT[2]}+1))"
+			
+			# MAC0317 - Introdução ao Processamento de Sinais Digitais
+			elif [ "$codigo_disciplina" = "0317" ]; then
+				dados[0]="$((${dados[0]}+1))"
+
+			# MAC0315 - Otimização Linear
+			elif [ "$codigo_disciplina" = "0315" ]; then
+				dados[2]="$((${dados[2]}+1))"
+				teoriaOBR[1]="$((${teoriaOBR[1]}+1))"
+			
+			# MAC0325 - Otimização Combinatória
+			elif [ "$codigo_disciplina" = "0325" ]; then
+				dados[2]="$((${dados[2]}+1))"
+				teoriaOBR[1]="$((${teoriaOBR[1]}+1))"
+				teoriaOPT[0]="$((${teoriaOPT[0]}+1))"
+
+			# MAC0427 - Otimização Não Linear
+			elif [ "$codigo_disciplina" = "0427" ]; then
+				dados[2]="$((${dados[2]}+1))"
+				teoriaOPT[1]="$((${teoriaOPT[1]}+1))"
+
+			# MAC0431 - Introdução à Computação Paralela e Distribuída
+			elif [ "$codigo_disciplina" = "0431" ]; then
+				dados[3]="$((${dados[3]}+1))"
+
+			# MAC0328 - Algoritmos em Grafos
+			elif [ "$codigo_disciplina" = "0328" ]; then
+				teoriaOBR[0]="$((${teoriaOBR[0]}+1))"
+
+			# MAC0327 - Desafios de Programação
+			elif [ "$codigo_disciplina" = "0327" ]; then
+				teoriaOPT[0]="$((${teoriaOPT[0]}+1))"
+
+			# MAC0331 - Geometria Computacional
+			elif [ "$codigo_disciplina" = "0331" ]; then
+				teoriaOPT[0]="$((${teoriaOPT[0]}+1))"
+
+			# MAC0336 - Criptografia para Segurança de Dados
+			elif [ "$codigo_disciplina" = "0336" ]; then
+				teoriaOPT[0]="$((${teoriaOPT[0]}+1))"
+
+			# MAC0385 - Estruturas de Dados Avançadas
+			elif [ "$codigo_disciplina" = "0385" ]; then
+				teoriaOPT[0]="$((${teoriaOPT[0]}+1))"
+
+			# MAC0450 - Algoritmos de Aproximação
+			elif [ "$codigo_disciplina" = "0450" ]; then
+				teoriaOPT[0]="$((${teoriaOPT[0]}+1))"
+				teoriaOPT[1]="$((${teoriaOPT[1]}+1))"
+
+			# MAC0465 - Biologia Computacional
+			elif [ "$codigo_disciplina" = "0465" ]; then
+				teoriaOPT[0]="$((${teoriaOPT[0]}+1))"
+				
+			# MAC0466 - Teoria dos Jogos Algorítmica
+			elif [ "$codigo_disciplina" = "0466" ]; then
+				teoriaOPT[0]="$((${teoriaOPT[0]}+1))"
+
+			# MAC0320 - Introdução à Teoria dos Grafos
+			elif [ "$codigo_disciplina" = "0320" ]; then
+				teoriaOBR[2]="$((${teoriaOBR[2]}+1))"
+
+			# MAC0436 - Tópicos de Matemática Discreta I
+			elif [ "$codigo_disciplina" = "0436" ]; then
+				teoriaOBR[2]="$((${teoriaOBR[2]}+1))"
+			fi
+
+		fi
+	fi
+
+	#printf ${lista_disciplinas[${i}]}" "
+	((i+=1));
+done
+
+
 
 
